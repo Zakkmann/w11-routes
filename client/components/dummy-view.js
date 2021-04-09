7388af93ed6b8b5c75c4bfe39b9513b0fe36ee48
Igor <zakkman@yandex.ru>
2021-04-10 00:16:30 +0300

    task01 is done

--- client/components/dummy-view.js
+++ client/components/dummy-view.js
@@ -1,13 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Head from './head'
 
 const Dummy = () => {
+  const [text, setText] = useState('')
+  const [textFromServer, setTextFromServer] = useState('')
+  console.log('You write: ', text)
+  
+  const click = () => {
+    fetch('api/v1/testString', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ input: text })
+    })
+      .then((res) => res.json())
+      .then((data) => setTextFromServer(data.result))
+  }
+
   return (
     <div>
       <Head title="Hello" />
       <div className="flex items-center justify-center h-screen">
-        <div className="bg-indigo-800 hover:text-red-500 text-white font-bold rounded-lg border shadow-lg p-10">
-          This is dummy component 1234
+        <div className="bg-indigo-800 text-white font-bold rounded-lg border shadow-lg p-10">
+          <div>Enter your text</div>
+            <div className="text-black">
+              <input type="text" onChange={(event) => setText(event.target.value)} value={text}/>
+            </div>
+            <div>
+              <button type="button" onClick={click}>Send!</button>
+            </div>
+            <div>
+              {textFromServer}
+            </div>
         </div>
       </div>
     </div>
--- server/server.js
+++ server/server.js
@@ -5,10 +5,13 @@ import bodyParser from 'body-parser'
 import sockjs from 'sockjs'
 import { renderToStaticNodeStream } from 'react-dom/server'
 import React from 'react'
-
+import axios from 'axios'
 import cookieParser from 'cookie-parser'
 import config from './config'
 import Html from '../client/html'
+
+
+const { writeFile, readFile, unlink } = require('fs').promises
 
 require('colors')
 
@@ -25,15 +28,210 @@ let connections = []
 const port = process.env.PORT || 8090
 const server = express()
 
+const setHeaders = (req, res, next) => {
+  res.set('x-skillcrucial-user', 'a523cc26-69aa-4ca0-ba95-7bb54a86f9b0')
+  res.set('Access-Control-Expose-Headers', 'X-SKILLCRUCIAL-USER')
+  next()
+}
+
 const middleware = [
   cors(),
   express.static(path.resolve(__dirname, '../dist/assets')),
   bodyParser.urlencoded({ limit: '50mb', extended: true, parameterLimit: 50000 }),
   bodyParser.json({ limit: '50mb', extended: true }),
-  cookieParser()
+  cookieParser(),
+  setHeaders
 ]
 
 middleware.forEach((it) => server.use(it))
+
+// подготовка с Пепе:
+// мой вариант:
+server.get('/api/v1/test1', (req, res) => {
+   axios('https://jsonplaceholder.typicode.com/users').then((usrs) => res.json(usrs.data))
+})
+
+// любопытная деструктуризация (Пепе говорит, что это лучший варик):
+server.get('/api/v1/test2', async(req, res) => {
+  const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+  res.json({ ...users })
+})
+
+// вариант Пепе с кэтчем, async/await и записью в файл:
+server.get('/api/v1/test3', async (req, res) => {
+  const url = 'https://jsonplaceholder.typicode.com/users'
+  const result = await axios(url)
+    .then((output) => output.data)
+    .catch((error) => ({ epicFail : error }))
+
+  writeFile(`${__dirname}/data/users.json`, JSON.stringify(result), { encoding: 'utf8'})
+
+  const str = await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+    .then((text) => JSON.parse(text))
+
+  console.log(str)
+
+  res.json(result)
+})
+
+// TASK 01:
+// get /api/v1/users - получает всех юзеров из файла users.json, если его нет - получает данные с сервиса
+// https://jsonplaceholder.typicode.com/users, заполняет файл users.json полученными данными и возвращает эти данные пользователю.
+
+// проверка файла на наличие и создание, если его нету
+server.get('/api/v1/users', async (req, res) => {
+  const users = await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+    .then((text) => {
+      console.log('File is found! =)')
+      return JSON.parse(text)
+    })
+    .catch(async () => {
+      console.log('File is not found ;(')
+      const output = await axios('https://jsonplaceholder.typicode.com/users')
+        .then((result) => {
+          writeFile(`${__dirname}/data/users.json`, JSON.stringify(result.data), { encoding: 'utf8'})
+          return result.data
+        })
+        .catch((err) => err)
+    return output
+  })
+  res.json(users)
+})
+
+// post /api/v1/users - добавляет юзера в файл users.json, с id равным id последнего элемента + 1 и возвращает { status: 'success', id: id }
+
+// МОИ СТРАДАНИЯ:
+// const fileSave = (info) => {
+//   writeFile(`${__dirname}/data/users.json`, JSON.stringify(info), { encoding: 'utf8'})
+// }
+
+// server.post('/api/v1/users', async (req, res) => {
+//   const users = await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+//     const newUser = req.body
+//     newUser.id = users[users.length].id + 1
+//     const usersUpd = [ ...users, newUser ]
+//     fileSave(usersUpd)
+//     res.json({ status: 'success', newUser })
+// })
+
+// Pepe КОД (адовый, с кэтчем отсутствия файла)
+server.post('/api/v1/users', async (req, res) => {
+  const newUser = req.body
+
+  const result = await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+    .then((text) => {
+      const users = JSON.parse(text)
+      newUser.id = users[users.length - 1].id + 1
+      const updUsers = [...users, newUser]
+      writeFile(`${__dirname}/data/users.json`, JSON.stringify(updUsers), { encoding: 'utf8'})
+      return { status: 'success', id: newUser.id }
+    })
+    .catch(async () => {
+      const parsing = await axios('https://jsonplaceholder.typicode.com/users')
+        .then(({ data: users }) => {
+          newUser.id = users[users.length - 1].id + 1
+          const updUsers = [...users, newUser]
+          writeFile(`${__dirname}/data/users.json`, JSON.stringify(updUsers), { encoding: 'utf8'})
+          return { status: 'success', id: newUser.id }
+        })
+        .catch((err) => err)
+    return parsing
+  })
+  res.json(result)
+})
+
+// patch /api/v1/users/:userId - получает новый объект, дополняет его полями юзера в users.json, с id равным userId, и возвращает { status: 'success', id: userId }
+
+server.get('/api/v1/users/:id', (req, res) => {
+  const { id } = req.params
+  // const id = req.params.id - идентичная запись, но ESLint ругается и хочет деструктуризацию ;(
+  console.log(req.params)
+  res.json({ thisIs: id })
+})
+
+
+server.patch('/api/v1/users/:userId', async (req, res) => {
+  const addData = req.body
+  const { userId } = req.params
+  await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+    .then((list) => {
+      const users = JSON.parse(list)
+      const updUsers = users.map((user) => {
+        if (user.id === +userId) {
+          return { ...user, ...addData }
+        }
+        return user
+      }) 
+      writeFile(`${__dirname}/data/users.json`, JSON.stringify(updUsers), { encoding: 'utf8'})
+    })
+    .catch((err) => console.log(err))
+  res.json({ status: 'success', id: userId })
+})
+
+// delete /api/v1/users/:userId - удаляет юзера в users.json, с id равным userId, и возвращает { status: 'success', id: userId }
+// проще всего сделать .map, но я сделал reduce
+
+server.delete('/api/v1/users/:userId', async (req, res) => {
+  const { userId } = req.params
+  await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+    .then((list) => {
+      const users = JSON.parse(list)
+      const filteredUsers = users.reduce((acc, rec) => (rec.id !== +userId) ? [...acc, rec] : acc, [])
+      console.log(userId)
+      writeFile(`${__dirname}/data/users.json`, JSON.stringify(filteredUsers), { encoding: 'utf8'})
+    })
+  .catch((err) => console.log(err))
+  res.json({ status: 'success', id: userId })
+})
+
+// delete /api/v1/users - удаляет файл users.json
+
+server.delete('/api/v1/users', (req, res) => {
+  unlink(`${__dirname}/data/users.json`)
+  res.json({ status: 'success' })
+})
+
+// TEMP:
+
+// const url01 = 'https://jsonplaceholder.typicode.com/users'
+
+// const readTaskFunc = async () => {
+//   await readFile(`${__dirname}/data/users.json`, { encoding: 'utf8'})
+//     .then((text) => JSON.parse(text))
+// }
+
+// const writeTaskFunc = async () => {
+//   await axios(url01)
+//     .then((result) => {
+//       writeFile(`${__dirname}/data/users.json`, JSON.stringify(result.data), { encoding: 'utf8'})
+//       return result.data
+//     })
+// }
+
+// server.get('/api/v1/users', async (req, res) => {
+//   await readTaskFunc().then(x => res.json(x))
+// })
+
+// server.post('/api/v1/testString', (req, res) => {
+//   const str = req.body.input.toUpperCase()
+//   res.json({ result: str })
+// })
+
+// server.get('/api/v1/users', async (req, res) => {
+//   const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')
+//   res.json(users)
+// })
+
+// server.get('/api/v1/users/take/:number', async (req, res) => {  
+//   const { number } = req.params  
+//   const { data: users } = await axios('https://jsonplaceholder.typicode.com/users')  
+//   res.json(users.slice(0, +number))  
+// })
+
+// server.get('/api/v1/users/:name', (req, res) => {
+//   const { name } = req.params
+//   res.json({ name })
+// })
 
 server.use('/api/', (req, res) => {
   res.status(404)
